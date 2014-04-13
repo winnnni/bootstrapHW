c4923eb0bf64abf0bd365769b3808238098b74b9
winnnni <jz2521@columbia.edu>
2014-04-13 12:27:42 -0400

    copy from class

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 /**
  * @author Winni Zhou
  */
+
+var video = "<iframe width=560' height='315' src='http://www.youtube.com/embed/o2-RXqU4Lg8?html5=1' frameborder='0' allowfullscreen></iframe>";
+$(document).ready(function() {
+	setNav();
+});
+
+function setNav(){
+	$(".cyclone").on("click", function() {
+		$('#myModal').modal();	
+		$('.modal-body').html(video);
+	});
+	
+	$(".close").on("click", function(){
+		$(".modal-body").empty();
+	});
+}
